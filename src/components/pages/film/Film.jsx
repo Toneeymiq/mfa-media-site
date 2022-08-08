2af7d4d5bcdecf4bc9039b23919112c7f63e8086
Miqkniq <89981841+Toneeymiq@users.noreply.github.com>
2022-08-09 00:59:08 +0300

    Update Film.jsx

--- src/components/pages/film/Film.jsx
+++ src/components/pages/film/Film.jsx
@@ -5,7 +5,7 @@ import HomeCss from '../../assets/css/homeCss/home.module.css'
 function FilmContent() {
   return (
     <div className={HomeCss.content}>
-      <div className={HomeCss.film_title}><h1>Videography</h1></div>
+      <div className={HomeCss.film_title}><h1>VIDEOGRAPHY</h1></div>
       {FilmLinks.map((value) => {
         return (
           <ContentBox
