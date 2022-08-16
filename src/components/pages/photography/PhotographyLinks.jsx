624ced86ddd2646d806f37fbb204aa9e0d2f4e1a
Toneeymiq <miqkniq@gmail.com>
2022-08-16 12:56:13 +0300

    changes to photography

--- src/components/pages/photography/PhotographyLinks.jsx
+++ src/components/pages/photography/PhotographyLinks.jsx
@@ -1,13 +1,6 @@
 const PhotographyLinks = [
   {
     link: "https://unsplash.com/@sparckey",
-    title: "Gimnple Low key",
-    Description: "Photo by Xavier Cee",
-    image:
-      "https://images.unsplash.com/photo-1658479558478-3456313f0096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  },
-  {
-    link: "https://unsplash.com/@sparckey",
     title: "Purple Flowers",
     Description: "Photo by Xavier Cee",
     image:
