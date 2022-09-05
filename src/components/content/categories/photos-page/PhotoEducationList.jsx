99f80cb3ab8875e3f163a2b5df03b2ce81711f81
Miqkniq <miqkniq@gmail.com>
2022-09-05 13:57:15 +0300

    Added Link for photos

--- src/components/content/categories/photos-page/PhotoEducationList.jsx
+++ src/components/content/categories/photos-page/PhotoEducationList.jsx
@@ -1,36 +1,54 @@
-import React from 'react'
+import React from "react";
 import HomeCss from "../../../assets/css/homeCss/home.module.css";
 
 import ContentBox from "../../Content-box";
 import VideoCss from "../../../assets/css/VideoCss/VideoCss.module.css";
 import AnimationCss from "../../../assets/css/overallCss/animations.module.css";
-
+import { BsArrowRight } from "react-icons/bs";
 
 function PhotoEducationList(props) {
-    return (
-        <>
-        <div className={VideoCss.category_title}>Education Photos</div>
-        <div className={`${HomeCss.content} ${VideoCss.film_container}`}>
-          {props.EducationLinks.map((value) => (
-              <ContentBox
-                title={value.title}
-                name={"By " + value.Name}
-                image={
-                  <div>
-                    <img src={value.image} alt={value.title} />
-                  </div>
-                }
-                link={
-                    <div className={`${VideoCss.photo_container} ${AnimationCss.tracking_in_expand}`}>
-                     { <img src={value.image} alt={value.title} />}
-                      <p>Visit Site</p>
+  return (
+    <>
+      <div className={VideoCss.category_title}>Education Photos</div>
+      <div className={`${HomeCss.content} ${VideoCss.film_container}`}>
+        {props.EducationLinks.map((value) => (
+          <ContentBox
+            title={value.title}
+            name={"By " + value.Name}
+            image={
+              <div>
+                <img src={value.image} alt={value.title} />
+              </div>
+            }
+            link={
+              <div
+                className={`${VideoCss.photo_container} ${AnimationCss.tracking_in_expand}`}
+              >
+                {<img src={value.image} alt={value.title} />}
+                <div className={VideoCss.Clickedimage_description}>
+                  <div className={VideoCss.clickedimage_title}>
+                    <p>{value.title}</p>
+
+                    <div className={VideoCss.external_link}>
+                      {" "}
+                      <h3>{value.Name}</h3>{" "}
+                      <a href={value.link} target="_blank" rel="noreferrer">
+                        {" "}
+                        <h2>
+                          Visit Site
+                          <BsArrowRight className={VideoCss.arrow} />{" "}
+                        </h2>
+                      </a>
                     </div>
-                  }
-              ></ContentBox>
-              ))}
-        </div>
-        </>
-      );
+                  </div>
+                </div>
+              </div>
+            }
+          ></ContentBox>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default PhotoEducationList
+export default PhotoEducationList;
--- src/components/content/categories/photos-page/PhotosSportsList.jsx
+++ src/components/content/categories/photos-page/PhotosSportsList.jsx
@@ -3,33 +3,51 @@ import HomeCss from "../../../assets/css/homeCss/home.module.css";
 import ContentBox from "../../Content-box";
 import VideoCss from "../../../assets/css/VideoCss/VideoCss.module.css";
 import AnimationCss from "../../../assets/css/overallCss/animations.module.css";
-
+import { BsArrowRight } from "react-icons/bs";
 
 function PhotoSportsList(props) {
-    return (
-      <>
+  return (
+    <>
       <div className={VideoCss.category_title}>Sports Photos</div>
-        <div className={`${HomeCss.content} ${VideoCss.film_container}`}>
-          {props.SportsLinks.map((value) => (
-              <ContentBox
-                title={value.title}
-                name={"By " + value.Name}
-                image={
-                  <div>
-                    <img src={value.image} alt={value.title} />
+      <div className={`${HomeCss.content} ${VideoCss.film_container}`}>
+        {props.SportsLinks.map((value) => (
+          <ContentBox
+            title={value.title}
+            name={"By " + value.Name}
+            image={
+              <div>
+                <img src={value.image} alt={value.title} />
+              </div>
+            }
+            link={
+              <div
+                className={`${VideoCss.photo_container} ${AnimationCss.tracking_in_expand}`}
+              >
+                {<img src={value.image} alt={value.title} />}
+                <div className={VideoCss.Clickedimage_description}>
+                  <div className={VideoCss.clickedimage_title}>
+                    <p>{value.title}</p>
+
+                    <div className={VideoCss.external_link}>
+                      {" "}
+                      <h3>{value.Name}</h3>{" "}
+                      <a href={value.link} target="_blank" rel="noreferrer">
+                        {" "}
+                        <h2>
+                          Visit Site
+                          <BsArrowRight className={VideoCss.arrow} />{" "}
+                        </h2>
+                      </a>
+                    </div>
                   </div>
-                }
-                link={
-                  <div className={`${VideoCss.photo_container} ${AnimationCss.tracking_in_expand}`}>
-                   { <img src={value.image} alt={value.title} />}
-                    <p>Visit Site</p>
-                  </div>
-                }
-              ></ContentBox>
-              ))}
-        </div>
-        </>
-      );
+                </div>
+              </div>
+            }
+          ></ContentBox>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default PhotoSportsList
+export default PhotoSportsList;
